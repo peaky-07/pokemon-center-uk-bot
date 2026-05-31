d3d1f0c1d27b76f375a1579a72ca344844afb831
peaky-07 <0jordandavies@gmail.com>
2026-05-31 19:11:19 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+from telegram import Bot
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+bot = Bot(token=BOT_TOKEN)
+
+bot.send_message(chat_id=CHAT_ID, text="Pokemon Center UK bot is working!")
