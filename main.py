59ccf61ffdfb5c1c463dbfb0f38ce1cfb529deb2
peaky-07 <0jordandavies@gmail.com>
2026-05-31 19:31:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
+import asyncio
 from telegram import Bot
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-bot = Bot(token=BOT_TOKEN)
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="Pokemon Center UK bot is working!"
+    )
 
-bot.send_message(chat_id=CHAT_ID, text="Pokemon Center UK bot is working!")
+asyncio.run(main())
